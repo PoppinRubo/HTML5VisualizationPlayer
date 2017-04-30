e6c7e2bd40e99d12313cd8e734c306b5afde2908
PoppinRubo <poppinrubo@163.com>
2017-04-30 23:27:12 +0800

    完成播放按键控制,下一首控制,列表播放

--- js/player.js
+++ js/player.js
@@ -1,21 +1,23 @@
 /**
- * HTML5 Audio Visualizer API
- * HTML5音频频谱可视化API
+ * HTML5 Audio Visualizer Player
+ * HTML5音乐可视化播放器
+ * 版本号:0.1.0.2017430_Alpha
  * Author：PoppinRubo
  * License: MIT
  */
 
 //创建一个对象方法
-function Visualizer() {
+function Player() {
     //先把自己用变量储存起来,后面要用
     var Myself = this;
     //频谱配置,外部调用就开始进行处理
     this.config = function (Object) {
-        Myself.audioUrl = Object.audioUrl;
+        Myself.playList = Object.playList;
         Myself.canvasId = Object.canvasId;
-        windowAudioContext();
+        //记录是否处理过音频,保证createMediaElementSource只创建一次,多次创建会出现错误
+        Myself.handle=0;
+        createParts();
     }
-    //下面这些为内部方法,外部不可访问
 
     //实例化一个音频类型window.AudioContext
     function windowAudioContext() {
@@ -25,66 +27,105 @@ function Visualizer() {
         window.cancelAnimationFrame = window.cancelAnimationFrame || window.webkitCancelAnimationFrame || window.mozCancelAnimationFrame || window.msCancelAnimationFrame;
         try {
             Myself.audioContext = new AudioContext();
-            //AudioContext实例化后就拉取音频
-            loadSound();
         } catch (e) {
             console.log(e + ',您的浏览器不支持 AudioContext');
         }
     }
 
+    //创建播放部件
+    function createParts() {
+        //创建audio
+        var audio = document.createElement("AUDIO");
+        var player = document.getElementById("player");
+        player.appendChild(audio);
+        Myself.audio = audio;
+        //创建控制按钮
+        var control = document.getElementById("playerControl");
+        control.innerHTML = '<i class="icon-play" data="play" id="playControl" title="播放">&#xeaa8</i><i class="icon-play" id="playNext" title="下一首">&#xeab0</i>';
+        var playControl = document.getElementById("playControl");
+        //播放,暂停
+        playControl.onclick = function () {
+            play();
+        }
+        //下一首
+        var playNext = document.getElementById("playNext");
+        playNext.onclick = function () {
+            next();
+        }
+        windowAudioContext();
+        loadSound();
+    }
+
     //加载音频对象方法
     function loadSound() {
-        var request = new XMLHttpRequest(); //建立一个请求
-        request.open('GET', Myself.audioUrl, true); //配置请求类型，文件路径,路径不可跨域
-        request.responseType = 'arraybuffer'; //配置数据返回类型
-        request.onload = function () {
-            //拉取成功返回ArrayBuffer(一块内存空间,缓冲),调用播放
-            play(request.response);
+        var playList = Myself.playList;
+        //把列表第一个mp3地址设置到audio上
+        Myself.audio.src = playList[0].mp3;
+        //记录当前播放在数组里的位置
+        Myself.nowPlay = 0;
+    }
+
+    //播放,暂停 控制
+    function play() {
+        var playBtn = document.getElementById("playControl");
+        var data = playBtn.getAttribute("data");
+        //字符图标变化
+        if (data == "play") {
+            playBtn.setAttribute("data", "pause");
+            playBtn.title = "暂停";
+            playBtn.innerHTML = "&#xeaa9";
+        } else {
+            playBtn.setAttribute("data", "play");
+            playBtn.title = "播放";
+            playBtn.innerHTML = "&#xeaa8"
         }
-        request.send();
+        //播放控制
+        if (Myself.audio.paused) {
+            Myself.audio.play();
+            //处理播放数据
+            if(Myself.handle==0){
+                playHandle();
+            }
+        } else {
+            Myself.audio.pause();
+        }
     }
 
-    //解码播放,提取数据
-    function play(audioData) {
+    //播放下一首
+    function next() {
+        //记录当前播放在数组里的位置位置移动,增加
+        Myself.nowPlay =Myself.nowPlay+1;
+        //取出mp3地址
+        Myself.audio.src = Myself.playList[Myself.nowPlay].mp3;
+        play();
+    }
+
+    //播放处理,提取数据
+    function playHandle() {
+        windowAudioContext();
         var audioContext = Myself.audioContext;
-        //解码ArrayBuffer
-        audioContext.decodeAudioData(audioData).then(function (decodedData) {
-            //进来到这里代表解码完成
-            //AudioBuffer接口表示存在存储器里的短音频资产，利用AudioContext.decodeAudioData()方法从音频文件构建，
-            // 或者利用 AudioContext.createBuffer()构建于原数据。
-            // 一旦将其放入AudioBuffer，可以传递到一个 AudioBufferSourceNode进行播放
-            var audioBufferSouceNode = audioContext.createBufferSource(),
-                analyser = audioContext.createAnalyser();
-            //将source与分析器连接
-            audioBufferSouceNode.connect(analyser);
-            //将分析器与destination连接，形成到达扬声器的通路
-            analyser.connect(audioContext.destination);
-            //将上面解码得到的decodedData数据赋值给source
-            audioBufferSouceNode.buffer = decodedData;
-            //播放,到这里就可以听到声音了
-            audioBufferSouceNode.start(0);
-            //AudioBufferSourceNode 接口代表一个由存储器中的音频数据组成的音频源,它通过AudioBuffer来进行存储. 它是一个AudioNode,现在把它存储一下
-            Myself.AudioBufferSourceNode=audioBufferSouceNode;
-            //接下来把分析器传出去创建频谱
-            drawSpectrum(analyser);
-        }, function (e) {
-            console.log(e + ",文件解码失败!");
-        });
-
+        var analyser = audioContext.createAnalyser();
+        var playData = audioContext.createMediaElementSource(Myself.audio);
+        // 将播放数据与分析器连接
+        playData.connect(analyser);
+        analyser.connect(audioContext.destination);
+        //接下来把分析器传出去创建频谱
+        drawSpectrum(analyser);
+        Myself.handle=1;
     }
+
     //画出频谱
     function drawSpectrum(analyser) {
         //颜色数组
-        var colorArray=['#f82466','#00FFFF','#AFFF7C','#FFAA6A','#6AD5FF','#D26AFF','#FF6AE6','#FF6AB8','#FF6A6A'];
+        var colorArray = ['#f82466', '#00FFFF', '#AFFF7C', '#FFAA6A', '#6AD5FF', '#D26AFF', '#FF6AE6', '#FF6AB8', '#FF6A6A'];
         //颜色随机数
-        var colorRandom=Math.floor(Math.random()*colorArray.length);
+        var colorRandom = Math.floor(Math.random() * colorArray.length);
         //效果随机数
-        var effectRandom=Math.floor(Math.random()*1);
+        var effectRandom = Math.floor(Math.random() * 1);
         //随机选取颜色
-        Myself.color=colorArray[colorRandom];
+        Myself.color = colorArray[colorRandom];
         //随机选取效果
-        switch(effectRandom)
-        {
+        switch (effectRandom) {
             case 0:
                 //条形
                 bar(analyser);
@@ -95,6 +136,7 @@ function Visualizer() {
         }
 
     }
+
     //条状效果
     function bar(analyser) {
         var canvas = document.getElementById(Myself.canvasId),
@@ -103,7 +145,7 @@ function Visualizer() {
             meterWidth = 10, //频谱条宽度
             gap = 2, //频谱条间距
             capHeight = 2,
-            capStyle = '#fff',
+            capStyle = '#FFFFFF',
             meterNum = 800 / (10 + 2), //频谱条数量
             capYPositionArray = []; //将上一画面各帽头的位置保存到这个数组
         ctx = canvas.getContext('2d'),
